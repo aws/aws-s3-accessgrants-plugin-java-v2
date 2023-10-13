649a4632869bd5131396daeb6c4e6610e053b0a4
Shiva Kumar Mukkapati <mshvkmr@amazon.com>
2023-10-13 01:25:47 -0400

    updating the utils class

--- src/main/java/software/amazon/awssdk/s3accessgrants/plugin/internal/S3AccessGrantsUtils.java
+++ src/main/java/software/amazon/awssdk/s3accessgrants/plugin/internal/S3AccessGrantsUtils.java
@@ -1,6 +1,7 @@
 package software.amazon.awssdk.s3accessgrants.plugin.internal;
 
 import software.amazon.awssdk.identity.spi.IdentityProperty;
+import software.amazon.awssdk.services.s3control.model.Privilege;
 import software.amazon.awssdk.utils.Validate;
 
 /**
@@ -10,6 +11,10 @@ public class S3AccessGrantsUtils {
 
     public static final IdentityProperty PREFIX_PROPERTY = IdentityProperty.create(String.class, "S3Prefix");
     public static final IdentityProperty OPERATION_PROPERTY = IdentityProperty.create(String.class,"Operation");
+
+    public static final Boolean DEFAULT_CACHE_SETTING = true;
+
+    public static final Privilege DEFAULT_PRIVILEGE_FOR_PLUGIN = Privilege.DEFAULT;
 
     public static void argumentNotNull(Object param, String message) {
         try{
