81457448afbd59684db6f1c972afe6993105876f
Shiva Kumar Mukkapati <mshvkmr@amazon.com>
2023-10-25 18:17:49 -0400

    Logging added for the plugin

--- src/main/java/software/amazon/awssdk/s3accessgrants/plugin/internal/S3AccessGrantsUtils.java
+++ src/main/java/software/amazon/awssdk/s3accessgrants/plugin/internal/S3AccessGrantsUtils.java
@@ -2,12 +2,15 @@ package software.amazon.awssdk.s3accessgrants.plugin.internal;
 
 import software.amazon.awssdk.identity.spi.IdentityProperty;
 import software.amazon.awssdk.services.s3control.model.Privilege;
+import software.amazon.awssdk.utils.Logger;
 import software.amazon.awssdk.utils.Validate;
 
 /**
  * The class is for defining all the utilities and constants to be used across the package
  * */
 public class S3AccessGrantsUtils {
+
+    public static Logger logger = Logger.loggerFor("S3Access Grants Plugin");
     public static final IdentityProperty PREFIX_PROPERTY = IdentityProperty.create(String.class, "S3Prefix");
     public static final IdentityProperty OPERATION_PROPERTY = IdentityProperty.create(String.class,"Operation");
 
